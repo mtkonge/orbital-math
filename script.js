0425618c4223c707beafedbf057feb8a6c1bebc4
MikLz69 <mtkongsted@gmail.com>
2022-05-24 14:24:15 +0200

    cleaned code and added custom

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict";
 const radius = document.getElementById("radius");
 const mass = document.getElementById("mass");
 const resultM = document.getElementById("result-m");
@@ -5,26 +6,30 @@ const resultKm = document.getElementById("result-km")
 const button = document.getElementById("button");
 const dropdown = document.getElementById("dropdown");
 
+const planetStringToValues = (planet) => {
+    const v = (mass, radius) => ({ mass, radius });
+    switch (planet) {
+        case 'jorden':
+            return v(5.972e+24, 6371000);
+        case 'mars':
+            return v(6.39e+23, 3379500);
+        case 'jupiter':
+            return v(1.898e+27, 69911000);
+        case 'solen':
+            return v(1.989e+30, 696340000);
+    }
+    throw new Error("dropdown overflow :)");
+}
+
+
 
 button.addEventListener("click", () => {
-    if (dropdown.value == "jorden") {
-        mass.value = 5.972e+24;
-        radius.value = 6371000;
-    }    
-    else if (dropdown.value == "mars") {
-        mass.value = 6.39e+23;
-        radius.value = 3379500;
+    if (dropdown.value != "custom") {
+        const planetValues = planetStringToValues(dropdown.value);
+        mass.value = planetValues.mass;
+        radius.value = planetValues.radius;
     }
-    else if (dropdown.value == "jupiter") {
-        mass.value = 1.898e+27;
-        radius.value = 69911000;
-    }
-    else if (dropdown.value = "solen") {
-        mass.value = 1.989e+30
-        radius.value =  696340000 
-
-    }
-    const result = Math.sqrt(6.67*10**-11*mass.value*(1/radius.value))
-    resultM.innerText = result + " m/s"
-    resultKm.innerText = result*3.6 + " km/t"
+    const result = Math.sqrt(6.67 * 10 ** -11 * mass.value * (1 / radius.value));
+    resultM.innerText = result + " m/s";
+    resultKm.innerText = result * 3.6 + " km/t";
 })
